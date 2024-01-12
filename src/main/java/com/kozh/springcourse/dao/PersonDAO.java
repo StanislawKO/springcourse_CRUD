35c8b5986f3759067674ff88ff89fc764344a027
Stanislaav Kozh <printks@yandex.ru>
2024-01-12 20:30:37 +0300

    add annotation @Valid

--- src/main/java/com/kozh/springcourse/dao/PersonDAO.java
+++ src/main/java/com/kozh/springcourse/dao/PersonDAO.java
@@ -14,10 +14,10 @@ public class PersonDAO {
     {
         people = new ArrayList<>();
 
-        people.add(new Person(++PEOPLE_COUNT, "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, "Bob"));
-        people.add(new Person(++PEOPLE_COUNT, "Mike"));
-        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+        people.add(new Person(++PEOPLE_COUNT, "Tom", 24, "tom@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob", 52, "bob@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike", 18, "mike@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy", 34, "katy@mail.ru"));
     }
 
     public List<Person> index() {
@@ -35,7 +35,10 @@ public class PersonDAO {
 
     public void update(int id, Person updatePerson) {
         Person personToBeUpdated = show(id);
+
         personToBeUpdated.setName(updatePerson.getName());
+        personToBeUpdated.setAge(updatePerson.getAge());
+        personToBeUpdated.setEmail(updatePerson.getEmail());
     }
 
     public void delete(int id) {
