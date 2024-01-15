eca0aace152f38f1ac53603d788887a108e87cad
Stanislaav Kozh <printks@yandex.ru>
2024-01-15 22:44:07 +0300

    add BatchUpdate, Spring Validator, @Pattern

--- src/main/java/com/kozh/springcourse/controller/PeopleController.java
+++ src/main/java/com/kozh/springcourse/controller/PeopleController.java
@@ -2,6 +2,7 @@ package com.kozh.springcourse.controller;
 
 import com.kozh.springcourse.dao.PersonDAO;
 import com.kozh.springcourse.models.Person;
+import com.kozh.springcourse.util.PersonValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,10 +16,12 @@ import javax.validation.Valid;
 public class PeopleController {
 
     private final PersonDAO personDAO;
+    private final PersonValidator personValidator;
 
     @Autowired
-    public PeopleController(PersonDAO personDAO) {
+    public PeopleController(PersonDAO personDAO, PersonValidator personValidator) {
         this.personDAO = personDAO;
+        this.personValidator = personValidator;
     }
 
     @GetMapping()
@@ -48,6 +51,8 @@ public class PeopleController {
     @PostMapping()
     public String create(@ModelAttribute("person") @Valid Person person,
                          BindingResult bindingResult) {
+        personValidator.validate(person, bindingResult);
+
         if (bindingResult.hasErrors()) {
             return "people/new";
         }
@@ -65,6 +70,8 @@ public class PeopleController {
     public String update(@ModelAttribute("person") @Valid Person person,
                          BindingResult bindingResult,
                          @PathVariable("id") int id) {
+        personValidator.validate(person, bindingResult);
+
         if (bindingResult.hasErrors()) {
             return "people/edit";
         }
